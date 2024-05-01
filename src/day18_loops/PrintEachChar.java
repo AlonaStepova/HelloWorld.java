941084d2ca458806f790cad6160a82f885ccffc3
Alona Stepova <safwanalona@gmail.com>
2024-05-01 18:58:36 -0400

    for i loops

---
+++ src/day18_loops/PrintEachChar.java
@@ -0,0 +1,32 @@
+package day18_loops;
+
+public class PrintEachChar {
+    public static void main(String[] args) {
+
+        String str = "loopcamp";
+
+        System.out.println(str.charAt(0));
+        System.out.println(str.charAt(1));
+        System.out.println(str.charAt(2));
+        System.out.println(str.charAt(3));
+
+
+        System.out.println("This is with the loop");
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i));
+
+            // [7 <= 8-1] --- [7 < 8]
+                        System.out.println(str.indexOf("x"));
+                        System.out.println(str.charAt(i));
+
+                    }
+
+
+
+
+
+                }
+            }
+
+
+
